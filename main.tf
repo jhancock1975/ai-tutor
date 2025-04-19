8e807b562c38bf43a7633ecaedee44595eabb959
John Hancock <jhancock1975@gmail.com>
2025-04-18 21:45:53 -0400

    add deploy for github actions and move index.html to a separate file

--- main.tf
+++ main.tf
@@ -162,34 +162,6 @@ resource "aws_api_gateway_deployment" "deploy" {
 resource "aws_s3_bucket_object" "index" {
   bucket = aws_s3_bucket.website.id
   key    = "index.html"
-
-  content = <<-HTML
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8"/>
-      <title>My AWS SPA</title>
-    </head>
-    <body>
-      <h1>Welcome to my AWS‑hosted SPA</h1>
-      <button id="sayHello">Say Hello</button>
-      <p id="msg"></p>
-      <script>
-        document.getElementById('sayHello').addEventListener('click', () => {
-          fetch("https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.deploy.stage_name}/hello")
-            .then(res => res.text())
-            .then(text => {
-              document.getElementById('msg').innerText = text;
-            })
-            .catch(err => {
-              console.error(err);
-              document.getElementById('msg').innerText = "Error calling API";
-            });
-        });
-      </script>
-    </body>
-    </html>
-  HTML
-
+  source = "${path.module}/index.html"
   content_type = "text/html"
 }
